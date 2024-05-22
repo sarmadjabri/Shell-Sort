498e0ddd8881c5d8674e2a37e81412b1d587893d
Sarmad Jabri <114747536+sarmadjabri@users.noreply.github.com>
2024-05-22 10:19:20 -0400

    Add files via upload

---
+++ Quick-Sort.py
@@ -0,0 +1,14 @@
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quicksort(left) + middle + quicksort(right)
+
+if __name__ == "__main__":
+    print("Enter a list of numbers separated by spaces:")
+    arr = list(map(int, input().split()))
+    sorted_arr = quicksort(arr)
+    print("Sorted array:", sorted_arr)
